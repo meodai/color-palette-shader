0c831608397f420caf7c0ed5d68763da00837464
David Aerne <meodai@gmail.com>
2024-04-20 23:37:04 +0200

    better demo

--- demo/main.js
+++ demo/main.js
@@ -2,15 +2,18 @@ import './style.css'
 
 import { PaletteViz } from '../src';
 
+const $palette = document.querySelector("[data-palette]");
+const $tools = document.querySelector("[data-tools]");
+
 const $app = document.querySelector("#app");
 let size = Math.min(window.innerWidth, window.innerHeight) * 0.6;
 
-const palette = [
-'#bc8b96', '#974b72', '#7f305c', '#5d2047', '#46173a', '#340d31', '#200816', 
-'#312234', '#40364a', '#5b596d', '#7c8497', '#9daec0', '#f8e6d0', '#dcbaa0', 
-'#c08e70', '#946452', '#683a34', '#442125', '#732f31', '#a23c3c', '#b45e4e', 
-'#cf8c52', '#e8c988', '#a3ab6d', '#5e8c51', '#436852', '#3e4350', '#381d4e',
-'#3c2c6a', '#444c84', '#5c79a6', '#8bc0ca',
+let palette = [
+  '#bc8b96', '#974b72', '#7f305c', '#5d2047', '#46173a', '#340d31', '#200816', 
+  '#312234', '#40364a', '#5b596d', '#7c8497', '#9daec0', '#f8e6d0', '#dcbaa0', 
+  '#c08e70', '#946452', '#683a34', '#442125', '#732f31', '#a23c3c', '#b45e4e', 
+  '#cf8c52', '#e8c988', '#a3ab6d', '#5e8c51', '#436852', '#3e4350', '#381d4e',
+  '#3c2c6a', '#444c84', '#5c79a6', '#8bc0ca',
  /*
   ...new Array(1000).fill(0).map((_, i) => {
     return `hsl(${Math.random() * 360}, ${Math.random() * 100}%, ${
@@ -19,10 +22,25 @@ const palette = [
   }),*/
 ];
 
+getNamesFromPalette(palette);
+
+function getNamesFromPalette(palette) {
+  fetch(
+    `https://api.color.pizza/v1/?values=${palette
+      .map((color) => color.slice(1))
+      .join(",")}&list=bestOf&noduplicates=true`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.colors);
+    });
+}
+
 const viz = new PaletteViz({
   palette,
   width: size,
   height: size,
+  $parent: $app,
 });
 
 const $hueSlider = document.createElement('input');
@@ -107,41 +125,56 @@ $inverseLightnessCheckbox.checked = false;
 
 $labelInverseLightness.appendChild($inverseLightnessCheckbox);
 
-const $colorList = document.createElement('div');
-
-$colorList.classList.add('color-list');
-
-$app.appendChild($colorList);
-
 window.addEventListener("resize", () => {
   viz.resize(Math.min(window.innerWidth, window.innerHeight) * 0.6);
 });
 
-palette.forEach((color) => {
-  const $picker = document.createElement("div");
-  $picker.style.setProperty("--color", color);
-  $picker.classList.add("color-picker");
+function createDomFromPalette (palette) {
+  $palette.innerHTML = "";
+  palette.forEach((color) => {
+    const $picker = document.createElement("div");
+    $picker.style.setProperty("--color", color);
+    $picker.classList.add("color-picker", "palette__color");
+    $picker.dataset.color = color;
 
-  const $pickerInput = document.createElement("input");
-  $pickerInput.type = "color";
-  $pickerInput.value = color;
-  $pickerInput.classList.add("color-picker-input");
-  $pickerInput.dataset.index = palette.indexOf(color);
+    const $pickerInput = document.createElement("input");
+    $pickerInput.type = "color";
+    $pickerInput.value = color;
+    $pickerInput.classList.add("color-picker-input");
+    $picker.dataset.index = palette.indexOf(color);
 
-  $picker.appendChild($pickerInput);
+    const $removeButton = document.createElement("button");
+    $removeButton.textContent = "x";
+    $removeButton.classList.add("color-picker__remove");
 
-  $colorList.appendChild($picker);
-
-  $pickerInput.addEventListener("input", (e) => {
-    const index = parseInt(e.target.dataset.index);
-    palette[index] = e.target.value;
-
-    viz.setColor(e.target.value, index);
-
-    $picker.style.setProperty("--color", e.target.value);
+    $picker.appendChild($pickerInput);
+    $picker.appendChild($removeButton);
+    $palette.appendChild($picker);
   });
+}
+
+$palette.addEventListener("input", (e) => {
+  // make sure the event comes from a color picker using the dataset
+  if (e.target.parentElement.dataset.index !== undefined) {
+    const $target = e.target;
+    const index = parseInt($target.parentElement.dataset.index);
+    $target.parentElement.style.setProperty("--color", $target.value);
+    $target.parentElement.dataset.color = $target.value;
+    viz.setColor($target.value, index);
+  }
+}, true);
+
+$palette.addEventListener("click", (e) => {
+  if (e.target.classList.contains("color-picker__remove")) {
+    const $target = e.target;
+
+    viz.removeColor(null, $target.parentElement.dataset.color);
+    createDomFromPalette(viz.palette);
+    $target.parentElement.remove();
+  }
 });
 
+createDomFromPalette(palette);
 
 $app.appendChild($hueSlider);
 
